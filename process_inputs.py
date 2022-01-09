9941685aae8ba42478dfb367ec0ada68416ea591
Moha-AlHanbali <moha.alhanbali@gmail.com>
2022-01-09 13:12:55 +0200

    feat: created oracles_extract and process_inputs modules

---
+++ process_inputs.py
@@ -0,0 +1,15 @@
+""" This module is based on analyze.ipynb and uses inputs from oracles_extract.py to run and return values"""
+
+def process_inputs(ally_team : list, enemy_team: list, champion : str):
+    """
+    process_inputs function recieves data from input_handler and runs analysis on match data to return results.
+
+        Arguments:
+            ally_team: User's team
+            enemy_team: The enemy team
+            champion: the User's champion
+
+        Return:
+            String: Analyzed item builds
+    """
+    pass
